2fae99e4d43a62bddf53391e6df1fb2523bb1cce
mmcgarry13 <mcgarry31@outlook.com>
2024-11-06 14:03:08 -0500

    Added some projects to resume page

--- src/pages/resumepage.jsx
+++ src/pages/resumepage.jsx
@@ -9,7 +9,23 @@ export default function ResumePage() {
                 <h3>Color my World</h3>
                 <p className="lead mb-0">
                     <a href="url">https://github.com/mmcgarry13/Color-My-World</a>
-                    This is a project made with Tim Rice and Josh Henley, it generates
+                    This is a project made with Tim Rice and Josh Henley, it takes user input for a base color then generates an 8 color theme based off that color,
+                    as well as recoloring the page based off the new theme and allows users to save color themes they like.
+                    Application is deployed through Github below:
+                </p>
+                <a href="url">https://mmcgarry13.github.io/Color-My-World/</a>
+            </div>
+        </div>
+        <div className="col-lg-4">
+            <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                <div className="features-icons-icon d-flex">
+                    <i className="bi-layers m-auto text-primary" />
+                </div>
+                <h3>README-Generator</h3>
+                <p className="lead mb-0">
+                    <a href="url">https://github.com/mmcgarry13/README-Generator</a>
+                    This is a project I made which runs in the terminal and takes user prompts to generate a README file, it is designed to be fast and convenient
+                    and create a usable README which any project should have.
                 </p>
             </div>
         </div>
